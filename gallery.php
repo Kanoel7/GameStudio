0f22eda69cf9be09e1c1c8097bfc5d0c29c01397
Kanoel7 <nhvwjfjbx@gmail.com>
2025-04-04 02:42:03 +0200

    footer.php

--- gallery.php
+++ gallery.php
@@ -71,58 +71,6 @@
             </div>
         </div>
     </div>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <!-- About -->
-                <div class="col-lg-3 col-md-6">
-                    <a class="text-decoration-none footer-title footer-links fs-4" href="about.html">About Us</a>
-                    <ul class="footer-links">
-                        <li><a href="about.html">History</a></li>
-                        <li><a href="about.html">Team</a></li>
-                        <li><a href="about.html">Our Mission</a></li>
-                    </ul>
-                </div>
-
-                <!-- Career -->
-                <div class="col-lg-3 col-md-6">
-                    <a class="text-decoration-none footer-title footer-links fs-4" href="careers.html">Career</a>
-                    <ul class="footer-links">
-                        <li><a href="careers.html">Jobs</a></li>
-                        <li><a href="careers.html">Internships</a></li>
-                        <li><a href="careers.html">Open Vacancies</a></li>
-                    </ul>
-                </div>
-
-                <!-- Contacts -->
-                <div class="col-lg-3 col-md-6">
-                    <a class="text-decoration-none footer-title footer-links fs-4" href="contact.html">Contacts</a>
-                    <ul class="footer-links">
-                        <li><a href="contact.html">Email</a></li>
-                        <li><a href="contact.html">Phone</a></li>
-                        <li><a href="contact.html">Address</a></li>
-                    </ul>
-                </div>
-
-                <!-- Social Media -->
-                <div class="col-lg-3 col-md-6">
-                    <a class="text-decoration-none footer-title footer-links fs-4" href="about.html">Social Media</a>
-                    <ul class="footer-links">
-                        <li><a href="about.html">Twitter</a></li>
-                        <li><a href="about.html">LinkedIn</a></li>
-                        <li><a href="about.html">Instagram</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Copyright -->
-            <div class="copyright">
-                <p>&copy; 2024 Gaming Studio. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    
-</body>
-</html>
+<?php 
+    include 'partials/footer.php'; 
+?>
---
+++ partials/footer.php
@@ -0,0 +1,42 @@
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row justify-content-between ms-5">
+                <div class="col-lg-4 ">
+                    <h5 class="text-white-50">Games</h5>
+                    <nav class="d-flex flex-column gap-2">
+                        <a href="#" class="nav-link text-white-50">Our Games</a>
+                        <a href="#" class="nav-link text-white-50">Upcoming Releases</a>
+                        <a href="#" class="nav-link text-white-50">Game Updates</a>
+                        <a href="#" class="nav-link text-white-50">Community Hub</a>
+                    </nav>
+                </div>
+                <div class="col-lg-4">
+                    <h5 class="text-white-50">Company</h5>
+                    <nav class="d-flex flex-column gap-2">
+                        <a href="about.php" class="nav-link text-white-50">About Us</a>
+                        <a href="careers.php" class="nav-link text-white-50">Careers</a>
+                        <a href="#" class="nav-link text-white-50">Press Kit</a>
+                        <a href="#" class="nav-link text-white-50">Investors</a>
+                    </nav>
+                </div>
+                <div class="col-lg-4">
+                    <h5 class="text-white-50">Support</h5>
+                    <nav class="d-flex flex-column gap-2">
+                        <a href="contact.php" class="nav-link text-white-50">Contact Us</a>
+                        <a href="#" class="nav-link text-white-50">Player Support</a>
+                        <a href="#" class="nav-link text-white-50">Bug Reports</a>
+                        <a href="#" class="nav-link text-white-50">Privacy Policy</a>
+                    </nav>
+                </div>
+            </div>
+            <hr class="border-secondary my-4">
+            <p class="text-center text-white-50 mb-0">© 2025 Gaming Studio. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
